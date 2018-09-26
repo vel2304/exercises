64bec4ad8b48e8b75845e0a493d0820f12323f04
Valeriia Rudyk <lera@mba.local>
2018-09-25 21:27:31 -0700

    added exercises 6 and 7

---
+++ 6_RetirementCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _6_RetirementCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your current age?  ");
+            int currentAge = GetUserInput();
+            Console.Write("At what age would you like to retire?  ");
+            int retireAge = GetUserInput();
+            int yearsLeft = retireAge - currentAge;
+            Console.WriteLine("You have {0} years left until you can retire.", yearsLeft);
+            int currentYear = DateTime.Now.Year;
+            if (currentAge > retireAge)
+            {
+                Console.WriteLine("You can already retire.");
+            }
+            else
+            {
+                int canRetireYear = currentYear + yearsLeft;
+                Console.WriteLine("It's {0}, so you can retire in {1}.", currentYear, canRetireYear);
+            }
+        }
+        static int GetUserInput()
+        {
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out int validNumber) || validNumber < 0)
+            {
+                Console.WriteLine("You should enter the positive number.");
+                input = Console.ReadLine();
+            }
+            return int.Parse(input);
+        }
+    }
+}
---
+++ 7_AreaOfRoom/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _7_AreaOfRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the length of the room in feet?  ");
+            double length = GetUserInput();
+            Console.Write("What is the width of the room in feet?  ");
+            double width = GetUserInput();
+            Console.WriteLine("You entered dimensions of {0} feet by {1} feet.", length, width);
+            double areaF = length * width;
+            const double TOSQUAREMETERS = 0.09290304;
+            double areaM = areaF * TOSQUAREMETERS;
+            Console.WriteLine("The area is\n{0} square feet\n{1:0.000} square meters", areaF, areaM);
+
+            //a new version that allows you to choose feet or meters for inputs
+            Console.WriteLine("_________________________________________");
+            Console.Write("If your measurement in feet - press f.\nIf your measurement in meters - press m.  ");
+
+            string chooseMeasurement = GetUserChoice();
+
+            if (chooseMeasurement == "f")
+            {
+                CalculateArea("feet");
+            }
+            else if (chooseMeasurement == "m")
+            {
+                CalculateArea("meters");
+            }
+        }
+        // Calculate Area
+        static void CalculateArea(string feetOrMeters)
+        {
+            Console.Write($"What is the length of the room in {feetOrMeters}?  ");
+            double length = GetUserInput();
+            Console.Write($"What is the width of the room in {feetOrMeters}?  ");
+            double width = GetUserInput();
+            Console.WriteLine($"You entered dimensions of {length} {feetOrMeters} by {width} {feetOrMeters}.");
+            double area = length * width;
+            Console.WriteLine($"The area is\n{area} square {feetOrMeters}");
+        }
+
+        // return "m" or "f"
+        static string GetUserChoice()
+        {
+            string input = Console.ReadLine();
+            while (input != "m" && input != "f")
+            {
+                Console.WriteLine("Please press f for feet or press m for meters on your keyboard.");
+                input = Console.ReadLine();
+            }
+            return input;
+        }
+
+        static double GetUserInput()
+        {
+            string input = Console.ReadLine();
+            while (!double.TryParse(input, out double validNumber) || validNumber < 0)
+            {
+                Console.WriteLine("Please, enter the positive number.");
+                input = Console.ReadLine();
+            }
+            return double.Parse(input);
+        }
+    }
+}
