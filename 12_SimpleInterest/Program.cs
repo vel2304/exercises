12330a8460b6d8cea04dd518f39883798223745b
Valeriia Rudyk <valeriia@mba.local>
2018-11-01 20:57:34 -0700

    added exercise 12

--- 12_SimpleInterest/Program.cs
+++ 12_SimpleInterest/Program.cs
@@ -8,18 +8,29 @@ namespace _12_SimpleInterest
         {
             Console.Write("Enter the principal:  ");
             decimal principalAmount = GetUserInput();
+
             Console.Write("Enter the annual rate of interest as a percentage (like 15, not .15):  ");
             decimal percentageRateOfInterest = GetUserInput();
             decimal rateOfIntetest = percentageRateOfInterest / 100;
+
             Console.Write("Enter the number of years:  ");
             decimal numberOfYears = GetUserInput();
+
             Console.WriteLine("\nAfter {0} years at {1:p1}, the investment will be worth {2:c}.", numberOfYears, rateOfIntetest, CalculateSimpleInterest(rateOfIntetest, principalAmount, numberOfYears));
+            
+            for (int i = 1; i < numberOfYears; i++)
+            {
+                decimal amountEachYear = principalAmount * (1 + rateOfIntetest * i);
+                Console.WriteLine("The investment at the end of year {0} will be {1}", i, amountEachYear);
+            }
         }
+        
         static decimal CalculateSimpleInterest(decimal rateOfIntetest, decimal principalAmount, decimal numberOfYears)
         {
             decimal amountAtTheEnd = principalAmount * (1 + rateOfIntetest * numberOfYears);
-            return Decimal.Ceiling(amountAtTheEnd);
+            return Decimal.Ceiling(amountAtTheEnd * 100) / 100;
         }
+        
         static decimal GetUserInput()
         {
             string input = Console.ReadLine();
