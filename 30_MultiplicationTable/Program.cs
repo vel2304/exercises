797db8fab262eb10ff5dded1b54e5d7f1e969275
Valeriia Rudyk <valeriia@mba.local>
2019-01-11 13:46:00 -0800

    Added exercise 30

---
+++ 30_MultiplicationTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _30_MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("|    ");
+            for (int i = 0; i <= 12; i++)
+            {
+                Console.Write("|{0, -4}", i);
+            }
+            Console.WriteLine("|");
+            Console.WriteLine("______________________________________________________________________");
+            for (int i = 0; i <= 12; i++)
+            {
+                Console.Write("|{0, -4}", i);
+                for (int j = 0; j <= 12; j++)
+                {
+                    int result = i * j;
+                    Console.Write("|{0, -4}", result);
+                }
+                Console.WriteLine("|");
+                Console.WriteLine("______________________________________________________________________");
+            }
+        }
+    }
+}
