e8b5b90ffeaa8253d4bf8c34805ecec9b4dee85a
Valeriia Rudyk <valeriia@mba.local>
2019-01-16 12:05:36 -0800

    Added exercise 33

---
+++ 33_Magic_8_ball/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _33_Magic_8_ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your question?   ");
+            string question = Console.ReadLine();
+
+            string [] answers = {"Most likely", "Ask again later", "Yes", "No", "Maybe", "Very doubtful",
+                                "It is decidedly so", "Sings point to yes"};
+
+            Random anyAnswer = new Random();
+            int answerIndex = anyAnswer.Next(answers.Length);
+
+            Console.WriteLine("{0}", answers[answerIndex]);
+        }
+    }
+}
